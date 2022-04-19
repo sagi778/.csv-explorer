a2b8e2cbdae9dee3b9b6060aeadb6b27551c4842
Sagi Cohen <85191320+sagi778@users.noreply.github.com>
2022-04-19 12:03:37 +0300

    Add files via upload

--- src/main/file_reader/Csv.java
+++ src/main/file_reader/Csv.java
@@ -1,17 +1,20 @@
 package main.file_reader;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 
 public class Csv {
 
-    private String file = "";
+    private File file = null;
+    private String path = "";
     private int rows = 0;
     private int columns = 0;
 
-    public Csv(String file){
+    public Csv(File file){
         file = file;
+        path = file.getPath();
         BufferedReader reader = null;
         String line = "";
 
@@ -20,9 +23,11 @@ public class Csv {
 
             while( (line = reader.readLine())!= null ){
                 String[] row = line.split(",");
+                rows += 1;
 
                 for(String index : row){
                     System.out.println(index);
+                    columns += 1;
                 }
                 System.out.println( );
             }
@@ -39,8 +44,13 @@ public class Csv {
         }
     }
 
-    public String getFile() {
+    public File getFile() {
         return file;
     }
-
+    public int getColumns() {
+        return columns;
+    }
+    public int getRows(){
+        return rows;
+    }
 }
