7156ca56294b6dbcaaedd94629086b55d7b53287
Sagi Cohen <85191320+sagi778@users.noreply.github.com>
2022-04-20 14:23:10 +0300

    Add files via upload

--- src/main/Controller.java
+++ src/main/Controller.java
@@ -1,14 +1,26 @@
 package main;
 
+import javafx.beans.InvalidationListener;
+import javafx.collections.ListChangeListener;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Tab;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 import javafx.stage.FileChooser;
-import javafx.stage.Window;
 import main.file_reader.Csv;
 
 import java.io.File;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
+import java.util.ListIterator;
+
+import main.file_reader.data_types.*;
+
+import javax.security.auth.callback.Callback;
 
 public class Controller {
 
@@ -22,6 +34,9 @@ public class Controller {
     TextField log;
 
     @FXML
+    TableView table;
+
+    @FXML
     void initialize(){}
 
     public void openAct(ActionEvent event) {
@@ -31,8 +46,20 @@ public class Controller {
 
         File file = fileChooser.showOpenDialog(null);
         if(file != null){
-            new Csv(file);
+            Csv df = new Csv(file);
             log.setText("Open File(" + file + ")");
+
         }
+    }
+
+    public void newAct(ActionEvent event) {
+        log.setText("New File");
+    }
+
+    public void addColumn(ActionEvent event) {
+        log.setText("New Column");
+        TableColumn col = new TableColumn("Column");
+        table.getColumns().addAll(col);
+
     }
 }
