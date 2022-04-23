e4c9b2d32ca5b7dffe590821aa5883fa125b1f8d
Sagi Cohen <85191320+sagi778@users.noreply.github.com>
2022-04-24 00:03:35 +0300

    Update Csv.java

--- src/main/file_reader/Csv.java
+++ src/main/file_reader/Csv.java
@@ -10,7 +10,7 @@ public class Csv {
     private File file = null;
     private String path = "";
     private int rows = 0;
-    private int columns = 0;
+    private String[] columns = null;
 
     public Csv(File file){
         file = file;
@@ -21,13 +21,15 @@ public class Csv {
         try{
             reader = new BufferedReader(new FileReader(file));
 
+            setColumns(reader); 
+
             while( (line = reader.readLine())!= null ){
                 String[] row = line.split(",");
-                rows += 1;
+                //rows += 1;
 
                 for(String index : row){
                     System.out.println(index);
-                    columns += 1;
+                    //columns += 1;
                 }
                 System.out.println( );
             }
@@ -44,12 +46,23 @@ public class Csv {
         }
     }
 
+    private void setColumns( BufferedReader reader) throws IOException {
+
+        String line = "";
+
+        while( (line = reader.readLine())!= null && this.columns == null ){
+            this.columns = line.split(",");
+        }
+    }
+
+    public String[] getColumns(){
+        return this.columns;
+    }
+
     public File getFile() {
         return file;
     }
-    public int getColumns() {
-        return columns;
-    }
+
     public int getRows(){
         return rows;
     }
